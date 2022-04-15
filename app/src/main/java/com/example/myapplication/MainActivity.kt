4dee62ee83a865f668c19c9a39ef7cb2aed2461c
Eryk <eryktkaczewski@gmail.com>
2022-04-15 13:20:13 +0200

    fix login bug

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -9,60 +11,60 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
 
-
-
    lateinit var auth: FirebaseAuth
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         auth = Firebase.auth
         val btnLogin = findViewById<Button>(R.id.login_btn)
-        val email = findViewById<EditText>(R.id.username)
+        val email = findViewById<EditText>(R.id.email)
         val password = findViewById<EditText>(R.id.password)
         val btnSignup = findViewById<Button>(R.id.btnSignup)
 
-        btnLogin.setOnClickListener{
-            //stop
-            //Toast.makeText(this, "Login Button", Toast.LENGTH_LONG).show()
+        btnLogin.setOnClickListener {
             val email2 = email.text.toString()
             val pass2 = password.text.toString()
 
-            /*
-            calling signInWithEmailAndPassword(email, pass)
-            function using Firebase auth object
-            On successful response Display a Toast
-            */
-
+            if (email2.isBlank() || pass2.isBlank()) {
+                Toast.makeText(
+                    baseContext, "Email or password can't be blank",
+                    Toast.LENGTH_SHORT
+                ).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else{
+                auth.signInWithEmailAndPassword(email2, pass2)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success")
+                            val user = auth.currentUser
+                            Toast.makeText(
+                                baseContext, "Authentication complete",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            val intent = Intent(this, UsersActivity2::class.java)
+                            startActivity(intent)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext, "Authentication failed, type again.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
         }
-
-        btnLogin.setOnClickListener {
-            Toast.makeText(this,"Hello !", Toast.LENGTH_SHORT).show()
-        }
-
         btnSignup.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
     }
 }
-
-/*val arrayAdapter: ArrayAdapter<*>
-val users = arrayOf(
-    "Virat Kohli", "Rohit Sharma", "Steve Smith",
-    "Kane Williamson", "Ross Taylor"
-)
-
-// access the listView from xml file
-var mListView = findViewById<ListView>(R.id.usersListView)
-arrayAdapter = ArrayAdapter(this,
-android.R.layout.simple_list_item_1, users)
-mListView.adapter = arrayAdapter*/
--- app/src/main/java/com/example/myapplication/SignupActivity.kt
+++ app/src/main/java/com/example/myapplication/SignupActivity.kt
@@ -26,40 +26,39 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-           val btnBacktologin = findViewById<Button>(R.id.btnBackToLogin)
-          btnBacktologin.setOnClickListener{
-            finish();
-          }
-
+        val btnBacktologin = findViewById<Button>(R.id.btnBackToLogin)
+        btnBacktologin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
         val email = findViewById<EditText>(R.id.newEmail)
         //val username = findViewById<EditText>(R.id.newUsername)
         val password = findViewById<EditText>(R.id.newPassword)
-
 
         auth = FirebaseAuth.getInstance()
 
         val btnSignUp = findViewById<Button>(R.id.btnMakeAcc)
         btnSignUp.setOnClickListener {
             val newemail = email.text.toString()
-           // val newusername = username.text.toString()
             val newpassword = password.text.toString()
 
             if (newemail.isBlank() || newpassword.isBlank()) {
                 Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-
-            auth.createUserWithEmailAndPassword(newemail,newpassword).addOnCompleteListener(this){
-                if(it.isSuccessful){
-                    Toast.makeText(this, "Successfully Singed Up", Toast.LENGTH_SHORT).show()
-                    finish();
-
-                }else {
-                    Toast.makeText(this, "Singed Up Failed!", Toast.LENGTH_SHORT).show()
-                }
+                val intent = Intent(this, SignupActivity::class.java)
+                startActivity(intent)
+            } else {
+                auth.createUserWithEmailAndPassword(newemail, newpassword)
+                    .addOnCompleteListener(this) {
+                        if (it.isSuccessful) {
+                            Toast.makeText(this, "Successfully Singed Up", Toast.LENGTH_SHORT)
+                                .show()
+                            finish();
+                        } else {
+                            Toast.makeText(this, "Singed Up Failed!", Toast.LENGTH_SHORT).show()
+                        }
+                    }
             }
         }
     }
 }
-
