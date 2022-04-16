36760762b09894fcb0a5d085d8122fceb9f90919
Miłosz <marzec1310@gmail.com>
2022-04-16 13:18:11 +0200

    Created splash screen ( no auto login / not working)

--- app/src/main/java/com/example/myapplication/SplashScreen.kt
+++ app/src/main/java/com/example/myapplication/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -7,22 +8,26 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.myapplication.databinding.ActivityTstBinding
+import com.example.myapplication.databinding.ActivitySplashScreenBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class Tst : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityTstBinding
+    private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityTstBinding.inflate(layoutInflater)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_tst)
+        val navController = findNavController(R.id.nav_host_fragment_content_splash_screen)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
@@ -30,10 +35,25 @@ class Tst : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        //Checking if user is already signed in
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }else {
+            val intent = Intent(this, UsersActivity2::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_tst)
+        val navController = findNavController(R.id.nav_host_fragment_content_splash_screen)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
